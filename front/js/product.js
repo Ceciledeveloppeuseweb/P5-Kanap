65e004ee9785917d982db4142d4ad9cb7c088b02
Cécile <alfonso.cilou@gmail.com>
2022-09-29 10:55:26 +0200

    modif page product.js page ok

--- front/js/product.js
+++ front/js/product.js
@@ -1,16 +1,24 @@
+/*[ on peut aussi faire :   
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const productId = urlParams.get("id");]
+*/
+// ********************************************************************************************************* //
+
 //=> redirection de la page produits à la page d'un produit via son id
 let productId = new URL(window.location.href).searchParams.get("id"); // on récupére l'id avec les paramétres de l'url
 //=> on récupère l'URL de la page active et on extrait l'ID d'un produit
 
 // ************************************ //
-//=> on récupère les infos du back-end
+//=> on récupère les infos du back-end depuis l'API
 fetch("http://localhost:3000/api/products/" + productId)
   .then((res) => res.json())
   .then((product) => getProduct(product)); //infos ds getProduct
 // ******************************************************************************* //
 //on récup les clés en variables ds arguments des fonctions
 function getProduct(kanap) {
-  const { imageUrl, altTxt, name, price, description, colors } = kanap;
+  const { imageUrl, altTxt, name, price, description, colors } = kanap; //=> destructuring = : const kanap = { imageUrl: kanap.Url, altTxt: kanap.altTxt, price: kanap.price, description: kanap.description, etc..}les clés sont des variables
+  //ou encore : const id=kanap._id/const imageUrl=kanap.imageUrl/const price=kanap.price/ etc...
   getImage(imageUrl, altTxt);
   getTitle(name);
   getPrice(price);
@@ -59,9 +67,9 @@ function getColors(colors) {
 //=> on envoie les produits sélectionnés au LS au clic du btn
 let btn = document.querySelector("#addToCart");
 btn.addEventListener("click", () => {
-  let elementsColors = document.querySelector("#colors").value; //=> récup de la couleur sélectionnée
-  console.log(elementsColors);
-  let basket = JSON.parse(localStorage.getItem(productId)) || []; //=>création du panier ds le LS encore vide/soit tab  soit tab vide
+  let couleurChoisie = document.querySelector("#colors").value; //=> récup de la couleur sélectionnée
+  console.log(couleurChoisie);
+  let basket = JSON.parse(localStorage.getItem("produits")) || []; //=>création du panier ds le LS encore vide/soit tab  soit tab vide
   console.log(basket);
   let quantity = document.querySelector("#quantity").value; //=>récup de la qté saisie
   console.log(quantity);
@@ -70,9 +78,9 @@ btn.addEventListener("click", () => {
     quantity == null ||
     quantity < 1 ||
     quantity > 100 ||
-    elementsColors === "" ||
-    elementsColors == null ||
-    elementsColors == undefined
+    couleurChoisie === "" ||
+    couleurChoisie == null ||
+    couleurChoisie == undefined
   )
     alert(
       "Veuillez sélectionner une couleur et une quantité (entre 1 et 100) du produit"
@@ -84,24 +92,23 @@ btn.addEventListener("click", () => {
   let newProduct = {
     id: productId,
     quantity: Number(quantity),
-    color: elementsColors,
+    color: couleurChoisie,
   };
   console.log(newProduct);
   //=> méthode pr rechercher un produit si  déja existant ds le LS
   let found = basket.find(
-    (element) => element.id == productId && element.color == elementsColors
+    (element) => element.id == productId && element.color == couleurChoisie
   );
-  console.log(productId);
-  console.log(elementsColors);
   if (found != undefined) {
-    let totalQuantity =
-      parseInt(found.quantity) + parseInt(newProduct.quantity); //=> valeur LS + value actuelle//transforme les chaines de caract en nbre
+    console.log(found);
+    let totalQuantity = parseInt(found.quantity) + parseInt(quantity); //=> valeur LS + value actuelle//transforme les chaines de caract en nbre
     found.quantity = totalQuantity;
-    console.log(found.quantity);
-    localStorage.setItem(productId, JSON.stringify(basket)); // => on enregistre et on additionne
+    
+    
   } else {
     basket.push(newProduct); //=> on enregistre les éléments ds le LS si il n'existe pas
-    localStorage.setItem(productId, JSON.stringify(basket)); // => on enregistre le nv pdt ds le LS
   }
+    
+  localStorage.setItem("produits", JSON.stringify(basket)); // => on enregistre le nv element et on additionne ds le LS
   console.log(found);
 });
