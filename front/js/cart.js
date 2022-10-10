a8aceda3353faff356b34031ec849bfe5a6465d7
Cécile <alfonso.cilou@gmail.com>
2022-10-10 12:03:45 +0200

    page cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,18 +4,12 @@
 // assemblage des informations des produits du panier et de l'API pour l'affichage
 // Boucle qui va récupérer les infos 'complètes' des produits du panier pour l'affichage
 // Affichage des produits du panier
-// ******************************************************************* //
+// ********************************************************************************************************************************************************** //
 
-// *********************** //
 let productsPanier = JSON.parse(localStorage.getItem("produits"));
-
 console.log(productsPanier); // => affiche les pdts du panier
 
-productsPanier.sort(function (a, b) {
-  return a.name - b.name;
-});
-//return (productsPanier(a.name) < productsPanier(b.name))?1:-1;
-
+// ********************************************************************************************************************************************************** //
 async function getProductsApi() {
   await fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
@@ -26,7 +20,7 @@ async function getProductsApi() {
 }
 getProductsApi();
 
-// ************************ //
+// ********************************************************************************************************************************************************** //
 
 function displayProducts(products) {
   let productComplete = [];
@@ -34,7 +28,7 @@ function displayProducts(products) {
   let totalPriceAllItems = 0;
   let totalQuantity = document.getElementById("totalQuantity");
   let totalQuantityAllItems = 0;
-  //let itemsQuantity = document.querySelectorAll(".itemQuantity")
+
   for (let index in productsPanier) {
     let productLs = productsPanier[index];
     let productFound = products.find(
@@ -66,7 +60,6 @@ function displayProducts(products) {
         </article>`;
     } //FIN CONDITION
 
-    console.log("/////");
     // CALCUL DU PRIX TOTAL DES ARTICLES COMMANDES
     let totalPriceItem = productFound.price * productLs.quantity;
     totalPriceAllItems += totalPriceItem;
@@ -86,13 +79,22 @@ function displayProducts(products) {
 
   //AFFICHAGE DE LA QUANTITE TOTALE DU NOMBRE D'ARTICLES COMMANDES
   totalQuantity.innerHTML = totalQuantityAllItems;
+  deleteItem();
   modifQuantity();
+  //deleteItem();
+
   //rangeProducts();
 } //FIN FONCTION
 
-console.log("********");
-// ********************************************** //
-
+// ********************************************************************************************************************************************************** //
+// fonction qui va prendre en compte le changement de la quantité
+// on sélectionne l'input où va s'effectuer le changement
+// on écoute l'évenement sur cet input et on récupère sa valeur
+// on parcours le produit qui a subit une modification de qté
+// on récupère le panier pour y enregistrer le changement
+// on récupère l'Id et la couleur du produit pour identifier le produit
+// on enregistre ds le LS la nouvelle qté du produit identifier
+// on raffraichit la page
 function modifQuantity() {
   const itemsQuantity = document.querySelectorAll(".itemQuantity");
   console.log("itemsQuantity", itemsQuantity);
@@ -112,160 +114,39 @@ function modifQuantity() {
           localStorage.setItem("produits", JSON.stringify(productsPanier)); // => on enregistre
         }
       }
-      window.location.reload();
+      window.location.reload(); // => réactualise la page
+    });
+  });
+}
+// ************************* //
+function deleteItem() {
+  const deleteButtons = document.querySelectorAll(".deleteItem");
+  deleteButtons.forEach((deleteButton) => {
+    deleteButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      const deleteId = event.target.getAttribute("id");
+      const deleteColor = event.target.getAttribute("data-color");
+      productsPanier = productsPanier.filter(
+        (element) => !(element.id == deleteId && element.color == deleteColor)
+        );
+        localStorage.removeItem("element");
+      console.log(productsPanier);
+      // Mise à jour du localStorage
+      localStorage.setItem("produits", JSON.stringify(productsPanier));
+      // Refresh de la page Panier
+      location.reload();
+      //alert("Article supprimé du panier.");
     });
   });
 }
 
-/*  function rangeProducts() {
-  productsPanier.sort(function (a, b) {
-   
-    if(a.name < b.name) {
-      return -1;
+
+
+
+
+       
       
-    }else{
-      return 1;
-    }
-    //return (productsPanier(a.name) < productsPanier(b.name))?1:-1;
-    
-  });
-  
-}  */
+      
+     
+      
 
-// ********************************************************************* //
-
-/* function changeQuantity() {
-  
-  let inputsQuantity = [];
-  console.log(inputsQuantity);
-  inputsQuantity.forEach((inputQuantity) => {
-    inputQuantity.addEventListener("change", () => {
-      let newQuantity = document.querySelectorAll(".itemQuantity").value;
-      console.log(newQuantity);
-      inputsQuantity.push(newQuantity);
-      console.log(newQuantity);
-      for (const productLs of productsPanier) {
-       productLs.quantity = parseInt(newQuantity)
-       inputQuantity.textContent = parseInt(newQuantity);
-     }
-     localStorage.setItem("produits", JSON.stringify(productsPanier));
-     //return newQuantity;
-    });
-  });
-  //inputQuantity.innerHTML = productLs.quantity;
-  //window.location.reload();
-}
-changeQuantity(); */
-
-// ********************* //
-/* function displayTotalPrice() {
-//  let total = 0;
-   const totalPrice = document.querySelector("#totalPrice");
-   const total = productsPanier.reduce((total, productLs) => total + productLs.price * productLs.quantity, 0);
-   totalPrice.textContent = Number(total);
-//   productsPanier.forEach(productLs => {
-//     const totalUnitPrice = parseInt(productLs.price) * parseInt(productLs.quantity);
-//     total += parseInt(totalUnitPrice);
-    
-//   });
-//   console.log(total);
-//   totalPrice.textContent = total;
-}
-displayTotalPrice() */
-
-// ************************************************* //
-// ***** //
-/* function getPanier() {
-  let produitsPanier = [];
-  if (localStorage.getItem("produits") != null) {
-    produitsPanier = JSON.parse(localStorage.getItem("produits")); //=> donne du  JSON data en objet js
-  }
-  return produitsPanier;
-} */
-
-// ******* //
-
-// function changeQuantity(products) {
-//   let totalQuantityAllItems = document.getElementById("totalQuantity");
-
-//   let inputs = document.querySelectorAll(".itemQuantity");
-
-//   productsPanier.forEach((productLs) => {
-//     inputs.forEach((input) => {
-//       input.addEventListener("change", () => {
-//         productLs.quantity = input.value;
-//         let quantityAmended = productLs.quantity;
-
-//         input.innerHTML += parseInt(quantityAmended);
-
-//         totalQuantityAllItems.innerHTML += parseInt(inputs);
-//         localStorage.setItem("produits", JSON.stringify(productsPanier));
-//         console.log(localStorage);
-//       });
-//     });
-//   });
-// }
-// changeQuantity();
-// *************************************************************** //
-/* function changeQuantity(productComplete) {
-  for (let i = 0; i < productsPanier.length; i++) {
-    const productLs = productsPanier[i];
-    
-  }
-  const allInputQuantity = document.querySelectorAll(".itemQuantity");// => récupération de l'input(de chaque produit)
-  allInputQuantity.forEach((inputQuantity) => {// => pour chaque input parmis ts les inputsQté
-    inputQuantity.addEventListener("change", (event) => {// => j'écoute la valeur rendue de l'input
-      event.preventDefault();//=> aucune action si pas d'évènement
-      const inputValue =  inputQuantity.value;//variable qui stock la valeur saisie
-      const dataId = productLs._id;// variable qui récupère l'id du produit
-      const dataColor = productLs.color;// variable qui récupère la couleur du pdt
-     console.log(inputValue);
-     console.log(dataId);
-     console.log( dataColor);
-     productsPanier = productLs.map((productLs, index) => {
-       if (productLs.id === dataId && productLs.color === dataColor) {
-         productLs.quantity = inputValue;
-       }
-       console.log(productLs);
-       return productLs;
-     });
-     // Mise à jour du localStorage
-     localStorage.setItem("produits", JSON.stringify(productsPanier));
-     // let productLsString = JSON.stringify(productsPanier);
-     // localStorage.setItem("produits", productLsString);
-     // Refresh de la page Panier
-    // location.reload();
-   });
- });
-} */
-//changeQuantity();
-
-// ************************************************ //
-
-// to delete items in cart
-// function deleteCartItem(productId, productColor) {
-//   let cartProducts = getCart();
-//   for (let i = 0; i < cartProducts.length; i++) {
-//     if(productId === cartProducts[i][0] && productColor === cartProducts[i][1]) {
-//       cartProducts.splice(i, 1);
-//       localStorage.setItem('cart', JSON.stringify(cartProducts));
-//       window.location.reload();
-//     }
-//   }
-// }
-
-// function changeQuantity(products) {
-//   let btnQuantity = 0;
-//   let btnItemsQuantity = document.querySelectorAll(".itemQuantity");
-
-//    btnItemsQuantity.addEventListener("change", (e) => {
-//     btnQuantity = parseInt(btnItemsQuantity.e.target.value);
-//     console.log(btnQuantity);
-//     productLs.quantity = parseInt(btnQuantity);
-//     console.log(btnQuantity);
-//     btnItemsQuantity += productLs.quantity
-//    })
-
-//    localStorage.setItem("produits", JSON.stringify(productsPanier));
-//  };
-//  changeQuantity()
